d40e1b4c299bfa3252f162a2121e57f02d4e6eec
Aris <arisansariofficial@gmail.com>
2025-09-13 16:47:26 +0530

    Auth handled and routes protected in client

--- backend/src/db/connectDB.js
+++ backend/src/db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      `${process.env.MONGO_URI}/${process.env.data}`
+      `${process.env.MONGO_URI}/${process.env.DB_NAME}`
     );
     console.log(`MongoDb connection successful: ${conn.connection.host}`);
   } catch (error) {
