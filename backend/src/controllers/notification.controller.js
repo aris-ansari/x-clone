1856bdfcf360a496fc8bf42b554c115afa68b99c
Aris <arisansariofficial@gmail.com>
2025-09-21 19:50:42 +0530

    notifications query and mutation added

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -11,7 +11,7 @@ export const getNotifications = async (req, res) => {
 
     await Notification.updateMany({ to: userId }, { read: true });
 
-    res.status(200).json({ success: true, data: notifications });
+    res.status(200).json(notifications);
   } catch (error) {
     console.error("Error in getNotifications controller", error.message);
     res.status(500).json({ success: false, message: "Something went wrong" });
