625a30839aaeac1d99d68cd12e22c693daee84bb
aris-ansari <125259227+aris-ansari@users.noreply.github.com>
2025-11-15 12:43:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://x-clone-backend-566u.onrender.com",
         changeOrigin: true,
       }
     }
